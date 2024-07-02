de1ae7fea53b525de0419cd5cf3c9eac86344991
SDRafael <j.rafael_jr@outlook.com>
2024-07-02 19:39:26 -0300

    first acessto mysql

---
+++ testandoQuery.py
@@ -0,0 +1,24 @@
+import mysql.connector
+# setando a conexão com o BD
+conexao = mysql.connector.connect(host='localhost',database='banco',user='root',password='jr.falaschi')
+
+# imprimindo a confirmação de conexão e estabelecendo o cursor
+if conexao.is_connected():
+    print('conectado com sucesso')
+    cursor = conexao.cursor()
+
+idade = int(input())
+
+if idade >= 18:
+    productName = input()
+    productValue = float(input())
+
+    cursor.execute(f'insert into vendas(produto, preço) value ("{productName}",{productValue});')
+    conexao.commit()
+else:
+    cursor.execute('SELECT * FROM vendas where preço < 100;')
+    read = cursor.fetchall()
+    print(read)
+# lembrar de sempre fechar as conexões independente dos resultados
+conexao.close()
+cursor.close()    
